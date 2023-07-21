fc7ab9736b429770ae8c8a06bd8ecfa6591e7c5d
Josep Maria Olivé Fernández <andugu@users.noreply.github.com>
2023-07-21 17:53:04 +0200

    Add wallet view

--- src/components/wallet.tsx
+++ src/components/wallet.tsx
@@ -20,19 +20,43 @@ const RechargeOptions = {
 export const Wallet = ({ name, balance }: UserProps) => {
 	return (
 		<Box sx={{backgroundColor: '#334d5c', borderRadius: 4}}>
-			<Box sx={{m: 2, p: 1}}>
-				<Box sx={{backgroundColor: '#d5d5d5', p: 1, borderRadius: 1}}>
-					<Box sx={{textAlign: 'right', fontSize: 15}}><b>{name}</b></Box>
+			<Box sx={{m: 2, paddingY: 2}}>
+				<Box sx={{backgroundColor: '#d5d5d5', p: 1, borderRadius: 2}}>
+					<Box sx={{textAlign: 'right', fontSize: 20}}><b>{name}</b></Box>
 				</Box>
-				<Box>
-					<Box>Add money</Box>
-					<Grid container rowSpacing={4} columnSpacing={{ xs: 1, sm: 2, md: 1 }}>
+				<Box sx={{backgroundColor: '#d5d5d5', mt: 3, paddingY: 3, paddingX: '10%', borderRadius: 2}}>
+					<Box sx={{mb: 2}}><b>Add money</b></Box>
+					<Grid container wrap="wrap" rowSpacing={4} columnSpacing={{xs: 1, sm: 3, md: 3}}>
 						{Object.keys(RechargeOptions).map((option) => (
-							<Grid item xs={6} sm={3}>
-								<Button sx={{backgroundColor: '#1e5d88', color: 'white', '&:hover': {backgroundColor: '#334d5c'}}}>{option}</Button>
+							<Grid item sm={6} md={4}>
+								<Box sx={{p: 1, m:1, display: 'flex', justifyContent: 'center'}}>
+									<Button size='large' sx={{
+										backgroundColor: '#1e5d88',
+										color: 'white',
+										display: 'block',
+										width: '100%',
+										'&:hover': {backgroundColor: '#334d5c'}
+									}}>{option}</Button>
+								</Box>
 							</Grid>
 						))}
 					</Grid>
+				</Box>
+				<Box sx={{backgroundColor: '#d5d5d5', mt: 3, paddingY: 3, borderRadius: 2}}>
+					<Box sx={{textAlign: 'center', fontSize: 30}}>
+						<b>Balance: {balance}€</b>
+					</Box>
+				</Box>
+				<Box sx={{backgroundColor: '#d5d5d5', mt: 3,  borderRadius: 2}}>
+					<Box sx={{textAlign: 'center', fontSize: 30}}>
+						<Button size='large' sx={{
+							backgroundColor: '#55ad7a',
+							color: 'white',
+							display: 'block',
+							width: '100%',
+							'&:hover': {backgroundColor: '#50ad7a'}
+						}}>Refund money</Button>
+					</Box>
 				</Box>
 			</Box>
 		</Box>
