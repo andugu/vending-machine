cb1ed434d25a99367b95ee54a9108831c00d494c
Josep Maria Olivé Fernández <andugu@users.noreply.github.com>
2023-07-24 19:45:11 +0200

    Move login to common

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -3,41 +3,42 @@ import {Navigate} from 'react-router-dom';
 import Box from "@mui/material/Box";
 import {InputAdornment, TextField} from "@mui/material";
 import Stack from '@mui/material/Stack';
-import Button from "@mui/material/Button";
 import AccountCircle from '@mui/icons-material/AccountCircle';
+import {LargeGreenButton} from "./common";
 
 export const Login = () => {
 
 	return (
-		<Box sx={{p: 1, m:1, display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh'}}>
-			<Stack spacing={3}>
-				<Box sx={{textAlign: 'center', fontSize: 30, mb: 2}}>
-					<b>Login</b>
-				</Box>
-				<Box>
-					<TextField
-						id="outlined-basic"
-						label="User"
-						variant="outlined"
-						InputProps={{
-							startAdornment: (
-								<InputAdornment position="start">
-									<AccountCircle />
-								</InputAdornment>
-							),
-						}}
-					/>
-				</Box>
-				<Box sx={{textAlign: 'center', fontSize: 30}}>
-					<Button size='large' sx={{
-						backgroundColor: '#55ad7a',
-						color: 'white',
-						display: 'block',
-						width: '100%',
-						'&:hover': {backgroundColor: '#50ad7a'}
-					}}>Submit</Button>
-				</Box>
-			</Stack>
+		<Box sx={{
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'center',
+			height: '100vh',
+			backgroundColor: '#1e5d88'}}>
+			<Box sx={{p: 8, backgroundColor: '#d5d5d5', borderRadius: 4}}>
+				<Stack spacing={3}>
+					<Box sx={{textAlign: 'center', fontSize: 30, mb: 2}}>
+						<b>Login</b>
+					</Box>
+					<Box>
+						<TextField
+							id="outlined-basic"
+							label="User"
+							variant="outlined"
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position="start">
+										<AccountCircle />
+									</InputAdornment>
+								),
+							}}
+						/>
+					</Box>
+					<Box>
+						{LargeGreenButton("Submit")}
+					</Box>
+				</Stack>
+			</Box>
 		</Box>
 	)
 };
