a2b2195a50271e4fd602ad1c824bc422e323d755
Josep Maria Olivé Fernández <andugu@users.noreply.github.com>
2023-07-24 19:42:42 +0200

    Move most buttons to common components

--- src/components/common.tsx
+++ src/components/common.tsx
@@ -1,4 +1,5 @@
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
 
 export enum MessageColorByType {
@@ -16,3 +17,28 @@ export const MainMessage = (message: string, messageColor: string) => {
 		</Box>
 	)
 };
+
+export const LargeGreenButton = (text: string) => {
+	return (
+		<Button size='large' sx={{
+			backgroundColor: '#55ad7a',
+			color: 'white',
+			display: 'block',
+			width: '100%',
+			'&:hover': {backgroundColor: '#50ad7a'}
+		}}>{text}</Button>
+	)
+}
+
+export const BlueButton = (text: string, size: string = 'medium') => {
+	return (
+		<Button sx={{
+			size: {size},
+			backgroundColor: '#1e5d88',
+			color: 'white',
+			display: 'block',
+			width: '100%',
+			'&:hover': {backgroundColor: '#334d5c'}
+		}}>{text}</Button>
+	)
+}
