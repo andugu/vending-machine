ef22f489335ba22c6be0dfbb8496608923344411
Josep Maria Olivé Fernández <andugu@users.noreply.github.com>
2023-07-25 16:42:39 +0200

    Add handle click for LargeGreenButton

--- src/components/common.tsx
+++ src/components/common.tsx
@@ -1,7 +1,6 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 
-
 export enum MessageColorByType {
 	ERROR = '#FF9494',
 	NOTICE = '#d5d5d5',
@@ -10,7 +9,7 @@ export enum MessageColorByType {
 
 export const MainMessage = (message: string, messageColor: string) => {
 	return (
-		<Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh'}}>
+		<Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
             <Box sx={{backgroundColor: messageColor, textAlign: 'center', fontSize: 30, p: 2, borderRadius: 2}}>
                 <b>{message}</b>
             </Box>
@@ -18,9 +17,9 @@ export const MainMessage = (message: string, messageColor: string) => {
 	)
 };
 
-export const LargeGreenButton = (text: string) => {
+export const LargeGreenButton = (text: string, handleClick = () => {}) => {
 	return (
-		<Button size='large' sx={{
+		<Button size='large' onClick={handleClick} sx={{
 			backgroundColor: '#55ad7a',
 			color: 'white',
 			display: 'block',
