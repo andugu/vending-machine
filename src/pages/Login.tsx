bc0946f0ca2d6163f0255a28a5253566cdae6122
Josep Maria Olivé Fernández <andugu@users.noreply.github.com>
2023-07-25 16:43:49 +0200

    Query redux store on login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,16 +1,14 @@
 import {FC} from 'react';
 import {Navigate} from 'react-router-dom';
+import { useSelector } from 'react-redux'
 
+import type { RootState } from '../store'
 import {Login} from "../components/login";
 
 export const LoginPage: FC = () => {
-
-	const user = false; // TODO connect to the redux store and retrieve the user name
-
-	// if user, no need to login again, redirect to vending machine home
-  if (user) {
-		return <Navigate to='/vending-machine' />
+	const user_name: string | null = useSelector((state: RootState) => state.wallet.user_name)
+	if (user_name !== null) {
+		return <Navigate to='/products' />
 	}
-
 	return <Login />
 }
