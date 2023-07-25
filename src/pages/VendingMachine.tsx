1c7417b3008d1c57ee50d4134840cd6f64923bcc
Josep Maria Olivé Fernández <andugu@users.noreply.github.com>
2023-07-25 16:44:59 +0200

    Move user back to login page if null on /products

--- src/pages/VendingMachine.tsx
+++ src/pages/VendingMachine.tsx
@@ -1,15 +1,16 @@
 import {FC} from 'react';
 import {Navigate} from 'react-router-dom';
+import {useSelector} from "react-redux";
+import Grid from "@mui/material/Grid";
 
+import {RootState} from "../store";
 import {Products} from "../components/product";
 import {Wallet} from "../components/wallet";
-import Grid from "@mui/material/Grid";
 
 
 export const VendingMachine: FC = () => {
-	const user = true; // TODO: Redirect to redux
-
-	if (!user) {
+	const user_name: string | null = useSelector((state: RootState) => state.wallet.user_name)
+	if (user_name === null) {
 		return <Navigate to='/' replace={true}/>
 	}
 
