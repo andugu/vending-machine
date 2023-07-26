beb538eadbb035bd20b0a164917d9932596c1a15
Josep Maria Olivé Fernández <andugu@users.noreply.github.com>
2023-07-26 18:31:56 +0200

    nit

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,11 @@
-import Axios from "axios"
+import axios from "axios"
 
 import {ProductProps, UserProps} from "./types";
 
 export const api = {
     login: async (user_name: string): Promise<UserProps> => {
         try {
-            const response = await Axios.post(process.env.REACT_APP_BACKEND_URL + '/login', {"user_name": user_name});
+            const response = await axios.post(process.env.REACT_APP_BACKEND_URL + '/login', {"user_name": user_name});
             return response.data
         } catch (error) {
             throw Error("Unable to create or recover user from API.")
@@ -13,14 +13,14 @@ export const api = {
     },
     logout: async (): Promise<void> => {
         try {
-            await Axios.get(process.env.REACT_APP_BACKEND_URL + '/logout');
+            await axios.get(process.env.REACT_APP_BACKEND_URL + '/logout');
         } catch (error) {
             throw Error("Unable to logout.")
         }
     },
     getProducts: async (): Promise<ProductProps[]> => {
         try {
-            const response = await Axios.get(process.env.REACT_APP_BACKEND_URL + '/products');
+            const response = await axios.get(process.env.REACT_APP_BACKEND_URL + '/products');
             return response.data
         } catch (error) {
             throw Error("Unable to recover products from API.")
@@ -28,7 +28,7 @@ export const api = {
     },
     buy: async (product_id: string): Promise<void> => {
         try {
-            const response = await Axios.post(process.env.REACT_APP_BACKEND_URL + '/buy', {"id": product_id});
+            const response = await axios.post(process.env.REACT_APP_BACKEND_URL + '/buy', {"id": product_id});
             return response.data
         } catch (error) {
             throw Error(`Unable to buy product in API: ${error}`)
@@ -36,7 +36,7 @@ export const api = {
     },
     patchBalance: async (user_id: string, new_balance: number): Promise<void> => {
         try {
-            await Axios.patch(process.env.REACT_APP_BACKEND_URL + `/profile?id=${user_id}`, {"balance": new_balance})
+            await axios.patch(process.env.REACT_APP_BACKEND_URL + `/profile?id=${user_id}`, {"balance": new_balance})
         } catch (error) {
             throw Error(`Unable to update balance for user with id: ${user_id}.`)
         }
