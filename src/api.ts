0c88fdd3da39d222f13f0909f9677dca4b5b514c
Josep Maria Olivé Fernández <andugu@users.noreply.github.com>
2023-07-26 10:25:56 +0200

    Add buy endpoint

--- src/api.ts
+++ src/api.ts
@@ -26,6 +26,14 @@ export const api = {
             throw Error("Unable to recover products from API.")
         }
     },
+    buy: async (product_id: string): Promise<void> => {
+        try {
+            const response = await Axios.post(process.env.REACT_APP_BACKEND_URL + '/buy', {"id": product_id});
+            return response.data
+        } catch (error) {
+            throw Error(`Unable to buy product in API: ${error}`)
+        }
+    },
     patchBalance: async (user_id: string, new_balance: number): Promise<void> => {
         try {
             await Axios.patch(process.env.REACT_APP_BACKEND_URL + `/profile?id=${user_id}`, {"balance": new_balance})
