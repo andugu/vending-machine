4b0faf805e6eff29a4a2b0d8ff581c0054a8e28e
Josep Maria Olivé Fernández <andugu@users.noreply.github.com>
2023-07-27 10:25:32 +0200

    Update API to work with CORS

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,18 @@
-import axios from "axios"
+import Axios, { AxiosInstance } from "axios"
 
 import {ProductProps, UserProps} from "./types";
+
+const axios: AxiosInstance = Axios.create({
+    baseURL: process.env.REACT_APP_BACKEND_URL,
+    xsrfCookieName: "csrftoken",
+    xsrfHeaderName: "X-CSRFTOKEN",
+    withCredentials: true
+});
 
 export const api = {
     login: async (user_name: string): Promise<UserProps> => {
         try {
-            const response = await axios.post(process.env.REACT_APP_BACKEND_URL + '/login', {"user_name": user_name});
+            const response = await axios.post('/login/', {"user_name": user_name});
             return response.data
         } catch (error) {
             throw Error("Unable to create or recover user from API.")
@@ -13,14 +20,14 @@ export const api = {
     },
     logout: async (): Promise<void> => {
         try {
-            await axios.get(process.env.REACT_APP_BACKEND_URL + '/logout');
+            await axios.get('/logout/');
         } catch (error) {
             throw Error("Unable to logout.")
         }
     },
     getProducts: async (): Promise<ProductProps[]> => {
         try {
-            const response = await axios.get(process.env.REACT_APP_BACKEND_URL + '/products');
+            const response = await axios.get('/products/');
             return response.data
         } catch (error) {
             throw Error("Unable to recover products from API.")
@@ -28,7 +35,7 @@ export const api = {
     },
     buy: async (product_id: string): Promise<void> => {
         try {
-            const response = await axios.post(process.env.REACT_APP_BACKEND_URL + '/buy', {"id": product_id});
+            const response = await axios.post('/buy/', {"id": product_id});
             return response.data
         } catch (error) {
             throw Error(`Unable to buy product in API: ${error}`)
@@ -36,7 +43,7 @@ export const api = {
     },
     patchBalance: async (user_id: string, new_balance: number): Promise<void> => {
         try {
-            await axios.patch(process.env.REACT_APP_BACKEND_URL + `/profile?id=${user_id}`, {"balance": new_balance})
+            await axios.patch(`/profile/${user_id}`, {"balance": new_balance})
         } catch (error) {
             throw Error(`Unable to update balance for user with id: ${user_id}.`)
         }
